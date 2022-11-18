a6a3dbdec436ac0facbaa577918e482b1d6dc2a7
Hein Htet <heinha123456@gmail.com>
2022-11-18 11:27:24 +0630

    working on card

--- src/components/order/SingleOrder.js
+++ src/components/order/SingleOrder.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./SingleOrder.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowsUpDown, faXmark } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowsUpDown,
+  faXmark,
+  faClock,
+} from "@fortawesome/free-solid-svg-icons";
+
+const getAmPmTime = (date) => {
+  let hour = date.getHours();
+  let amPm = "AM";
+  const minute = date.getMinutes();
+  if (hour > 12) {
+    hour = hour - 12;
+    amPm = "PM";
+  }
+  const string = `${hour}:${minute} ${amPm}`;
+  return string;
+};
 
 const SingleOrder = () => {
   return (
@@ -9,8 +25,21 @@ const SingleOrder = () => {
       <div className="single-order-container">
         <ul className="order-ul">
           <li className="name-count-container">
-            <div>chinese fried rice</div>
-            <div>x 2</div>
+            <div className="food-name">
+              <input defaultValue={"chinese fried rice"} placeholder="Name" />
+            </div>
+            <div>x</div>
+            <div className="food-count">
+              <select name="foodCount" id="foodCount">
+                <option value={1}>1</option>
+                <option value={1}>2</option>
+                <option value={1}>3</option>
+                <option value={1}>4</option>
+                <option value={1}>5</option>
+                <option value={6}>Others...</option>
+              </select>
+              <input type="text" style={{ display: "none" }} />
+            </div>
           </li>
           <li>
             <div>Message</div>
@@ -31,12 +60,16 @@ const SingleOrder = () => {
           <li>
             <div>Status</div>
             <div>:</div>
-            <div className="custom-select">
+            <div className="status-select">
               <select>
                 <option value="orderReceived">Order Received</option>
                 <option value="orderAccepted">Order Accepted</option>
                 <option value="delivered">Delievered</option>
               </select>
+              <div className="status-time">
+                <FontAwesomeIcon icon={faClock} />
+                <span>{getAmPmTime(new Date())}</span>
+              </div>
             </div>
           </li>
           <li>
