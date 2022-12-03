3673e952a6e3f73db2a0df0a810016a9bc67c899
Hein Htet <heinha123456@gmail.com>
2022-12-03 22:48:16 +0630

    before menu animation remove

--- src/components/order/OrderStates/ConnectionError.js
+++ src/components/order/OrderStates/ConnectionError.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./EmptyOrder.css";
 import "./ConnectionError.css";
+import { useOrderContext } from "../../../Context/OrderContext";
 
 const ConnectionError = () => {
+  const { setOrderState } = useOrderContext();
+  const controller = new AbortController();
+
+  // useEffect(() => {
+  //   return () => controller.abort();
+  // }, []);
+
   return (
     <div className="empty-box">
-      <button>Try Again ?</button>
+      <button onClick={() => setOrderState(controller)}>Try Again ?</button>
     </div>
   );
 };
